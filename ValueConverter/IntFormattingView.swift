a749cf56a2e9a6101efc7a6f1a5082bf1707af87
Uli Kusterer <ulivc@zathras.de>
2024-04-23 22:13:27 +0200

    Add some hex-string conversion code.

--- ValueConverter/IntFormattingView.swift
+++ ValueConverter/IntFormattingView.swift
@@ -20,6 +20,30 @@ enum FormattingView {
 		}
 	}
 	
+	static func fromHexToBytes(_ hexString: String) -> [UInt8] {
+		var hexStringToParse = hexString
+		if (hexString.count % 2) != 0 {
+			hexStringToParse += "0"
+		}
+		var bytes = [UInt8]()
+		var currIndex = hexString.startIndex
+		while currIndex < hexStringToParse.endIndex {
+			let nextIndex = hexStringToParse.index(currIndex, offsetBy: 2)
+			let hexByteStr = hexStringToParse[currIndex..<nextIndex]
+			bytes.append(UInt8(hexByteStr, radix: 16) ?? 0)
+			currIndex = nextIndex
+		}
+		return bytes
+	}
+
+	static func fromBytesToHex(_ bytes: [UInt8]) -> String {
+		var str = ""
+		for currCharacter in bytes {
+			str += String(format: "%02X", arguments: [UInt32(currCharacter)])
+		}
+		return str
+	}
+	
 }
 
 struct IntFormattingView<T: LosslessStringConvertible & ExpressibleByIntegerLiteral & Equatable>: View {
--- ValueConverterTests/ValueConverterTests.swift
+++ ValueConverterTests/ValueConverterTests.swift
@@ -12,8 +12,16 @@ final class ValueConverterTests: XCTestCase {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
 	}
 	
-	func testExample() throws {
+	func testHexString() throws {
+		let bytes: [UInt8] = [1, 2, 32, 33]
+		XCTAssertEqual(FormattingView.fromBytesToHex(bytes), "01022021")
 		
+		XCTAssertEqual(FormattingView.fromHexToBytes("01022021"), bytes)
+		
+		let paddedBytes: [UInt8] = [1, 2, 32, 32]
+		XCTAssertEqual(FormattingView.fromHexToBytes("0102202"), paddedBytes)
+
+		XCTAssertEqual(FormattingView.fromHexToBytes(""), [])
 	}
 	
 }
