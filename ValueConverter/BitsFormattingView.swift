3b20a6b755488db50756bad727e88a7486cbec7d
Uli Kusterer <ulivc@zathras.de>
2024-04-25 21:03:59 +0200

    Give us bits for all 8 bits and arrange them in a slightly more pleasant way.

--- ValueConverter/BitsFormattingView.swift
+++ ValueConverter/BitsFormattingView.swift
@@ -24,37 +24,62 @@ struct BitsFormattingView: View {
 	let placeholder: String
 	@Binding var model: FormattingView.ViewModel
 	
-	private func bit(from bitIndex: Int) -> UInt8 {
-		return 1 << (7 - bitIndex)
-	}
-	
 	var body: some View {
 		LabeledContent("Bits") {
 			VStack {
 				HStack {
-					ForEach(0..<4) { byteIndexUnflipped in
-						let byteIndex = 3 - byteIndexUnflipped
-						ForEach(0..<8) { bitIndex in
-							Toggle("\((byteIndex * 8) + (7 - bitIndex))", isOn: Binding(
-								get: {
-									let byte = model.rawBytes[safe: byteIndex] ?? 0
-									return (byte & bit(from: bitIndex)) == bit(from: bitIndex)
-								},
-								set: { newValue in
-									let byte = model.rawBytes[safe: byteIndex] ?? 0
-									let oldValue = (byte & bit(from: bitIndex)) == bit(from: bitIndex)
-									if newValue != oldValue {
-										model.rawBytes[safe: byteIndex] = newValue ? (byte | bit(from: bitIndex)) : (byte & ~bit(from: bitIndex))
-									}
-								}
-							))
-							.toggleStyle(.checkbox)
-						}
-						Spacer(minLength: 20.0)
-					}
+					BitView(model: $model, byteIndex: 1)
+					Spacer(minLength: 20.0)
+					BitView(model: $model, byteIndex: 0)
+				}
+				HStack {
+					BitView(model: $model, byteIndex: 3)
+					Spacer(minLength: 20.0)
+					BitView(model: $model, byteIndex: 2)
+				}
+				HStack {
+					BitView(model: $model, byteIndex: 5)
+					Spacer(minLength: 20.0)
+					BitView(model: $model, byteIndex: 4)
+				}
+				HStack {
+					BitView(model: $model, byteIndex: 7)
+					Spacer(minLength: 20.0)
+					BitView(model: $model, byteIndex: 6)
 				}
 			}
 		}
 	}
 }
 
+struct BitView: View {
+	
+	@Binding var model: FormattingView.ViewModel
+	let byteIndex: Int
+	
+	private func bit(from reverseBitIndex: Int) -> UInt8 {
+		return 1 << (7 - reverseBitIndex)
+	}
+	
+	var body: some View {
+		HStack {
+			ForEach(0..<8) { bitIndex in
+				Toggle("\((byteIndex * 8) + (7 - bitIndex))", isOn: Binding(
+					get: {
+						let byte = model.rawBytes[safe: byteIndex] ?? 0
+						return (byte & bit(from: bitIndex)) == bit(from: bitIndex)
+					},
+					set: { newValue in
+						let byte = model.rawBytes[safe: byteIndex] ?? 0
+						let oldValue = (byte & bit(from: bitIndex)) == bit(from: bitIndex)
+						if newValue != oldValue {
+							model.rawBytes[safe: byteIndex] = newValue ? (byte | bit(from: bitIndex)) : (byte & ~bit(from: bitIndex))
+						}
+					}
+				))
+				.toggleStyle(.checkbox)
+			}
+		}
+	}
+
+}
